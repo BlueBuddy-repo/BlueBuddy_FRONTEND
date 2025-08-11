d70ee965dcaa56ae740a564d231ca3534657b075
Minseon Jeon <mminnn28@gmail.com>
2025-08-12 02:21:51 +0900

    [FEAT] 나의 정보 수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Section/Footer'
 import Home from './components/Home/Home'
 import Map from './components/Map/Map';
 import MyPage from './components/MyPage/MyPage';
+import MyInfo from './components/MyPage/MyInfo';
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/map" element={<Map />}></Route>
                 <Route path="/mypage" element={<MyPage />}></Route>
+                <Route path="/myinfo" element={<MyInfo />}></Route>
             </Routes>
             <Footer />
         </BrowserRouter>
