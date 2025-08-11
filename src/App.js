43c6dd308f295de563226bebf6a85a3fb2a3372f
sujin <csujin2040@gmail.com>
2025-08-11 22:47:29 +0900

    Feat: 지도 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Section/Header'
 import Footer from './components/Section/Footer'
 import Home from './components/Home/Home'
+import Map from './components/Map/Map';
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+               		<Route path="/" element={<Home />}></Route>
+                    <Route path="/map" element={<Map />}></Route>
+                    
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App
--- src/components/Section/Footer.jsx
+++ src/components/Section/Footer.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
 import Home from '../../assets/img/icon/home.svg'
 import Book from '../../assets/img/icon/book.svg'
-import Map from '../../assets/img/icon/home.svg'
+import Map from '../../assets/img/icon/map.svg'
 import Mypage from '../../assets/img/icon/mypage.svg'
 
+import HomeOn from '../../assets/img/icon/home_on.svg'
+import BookOn from '../../assets/img/icon/book_on.svg'
+import MapOn from '../../assets/img/icon/map_on.svg'
+import MypageOn from '../../assets/img/icon/mypage_on.svg'
+
 const Footer = () => {
+    const location = useLocation()
+    const path = location.pathname
+
     return (
         <div className='footer_wrap'>
             <div className="btns">
-                <img src={Home} alt="home" />
-                <img src={Book} alt="book" />
-                <img src={Map} alt="map" />
-                <img src={Mypage} alt="mypage" />
+                <Link to="/home">
+                    <img src={path === '/home' ? HomeOn : Home} alt="home" />
+                </Link>
+                <Link to="/book">
+                    <img src={path === '/book' ? BookOn : Book} alt="book" />
+                </Link>
+                <Link to="/map">
+                    <img src={path === '/map' ? MapOn : Map} alt="map" />
+                </Link>
+                <Link to="/mypage">
+                    <img src={path === '/mypage' ? MypageOn : Mypage} alt="mypage" />
+                </Link>
             </div>
-
         </div>
     )
 }
