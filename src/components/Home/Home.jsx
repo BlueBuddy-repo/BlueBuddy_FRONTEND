f23c6ed11f1e08f6035d3478b3ebaa811bdd9a47
yyen <brenda1217@naver.com>
2025-08-14 02:36:34 +0900

    [feat] 홈 연동

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import waveBox from '../../assets/img/home/waveBox.png';
 import wave from '../../assets/img/home/wave.png';
 import balloon from '../../assets/img/home/speech_balloon.png';
-import shark from '../../assets/img/home/shark.png';
 import heart from '../../assets/img/home/heart.png';
 
 const Home = () => {
+
+  const [petName, setPetName] = useState('');
+  const [petImage, setPetImage] = useState('');
+  const [waveCount, setWaveCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+       try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          console.error('토큰이 없습니다. 로그인 후 이용하세요.');
+          return;
+        }
+
+        const [petRes, waveRes] = await Promise.all([
+          axios.get(`${process.env.REACT_APP_API_URL}/user-creatures/getMyPet`, {
+            headers: { Authorization: `Bearer ${token}` }
+          }),
+          axios.get(`${process.env.REACT_APP_API_URL}/wave/getWave`, {
+            headers: { Authorization: `Bearer ${token}` }
+          })
+        ]);
+
+        setPetName(petRes.data.data.petName);
+        setPetImage(petRes.data.data.petImage);
+
+        setWaveCount(waveRes.data.data);
+
+      } catch (err) {
+        console.error('데이터 불러오기 실패:', err);
+      }
+    };
+
+    fetchData();
+  }, [token]);
+
+  
+
   return (
     <div className='home_wrap contents'>
       <div className="wave_box">
         <img className="img" src={waveBox} alt="wave" />
-        <div className="pet_name">블루</div>
+        <div className="pet_name">{petName}</div>
         <div className="wave_index">
           <img className="wave_icon" src={wave} alt="물결" />
-          <span>325</span>
+          <span>{waveCount}</span>
         </div>      
       </div>
 
       <div className='creature'>        
-        <img className="img" src={shark} alt="creature" />
+        {petImage && <img className="img" src={petImage} alt="creature" />}
         <img className="heart" src={heart} alt="heart" />
       </div>
       <div className='balloon'>
